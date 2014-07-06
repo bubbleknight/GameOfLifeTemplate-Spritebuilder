e8449e26ec54ad403fcc0c7202ee224d80a0e227
bubbleknight <sungchou@Sungs-MacBook-Pro.local>
2014-07-05 23:31:40 -0500

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -154,10 +154,12 @@ static const int GRID_COLUMNS = 10;
             
             if (currentCreature.livingNeighbors == 3) {
                 currentCreature.isAlive = TRUE;
-                numAlive += 1;
-                
             } else if (currentCreature.livingNeighbors <= 1 || currentCreature.livingNeighbors >= 4) {
                 currentCreature.isAlive = FALSE;
+            }
+            
+            if (currentCreature.isAlive) {
+                numAlive += 1;
             }
         }
     }
